21da0b07a8bd9feb8df91078a353c5a933f40a51
miroslav-savic <miroslav>
2021-04-19 22:38:39 +0200

    implement connect and mapToDispatch functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 
@@ -46,14 +46,18 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}></Route>
           <Route path='/shop' component={ShopPage}></Route>
-          <Route path='/signin' component={SignInSignUp}></Route>
+          <Route exact path='/signin' render={() => this.props.currentUser ? ( <Redirect to='/' />) : (<SignInSignUp />)}></Route>
         </Switch>
     </div>
     )}
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
